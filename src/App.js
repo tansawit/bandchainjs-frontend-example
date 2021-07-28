66e1c4bec7236fedf20e80dc66759573082f4b3a
Sawit Trisirisatayawong <sawit@bandprotocol.com>
2021-07-28 14:24:25 +0700

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,22 +1,26 @@
 import logo from './logo.svg';
 import './App.css';
+import { useEffect, useState } from 'react';
+import { Client } from '@bandprotocol/bandchain.js';
+
+async function getPrice(setPrice) {
+  console.log('test2');
+  const endpoint = 'https://api-gm-lb.bandchain.org';
+  const client = new Client(endpoint);
+  const rate = await client.getReferenceData(['BTC/USD']);
+  setPrice(rate[0].rate);
+}
 
 function App() {
+  const [price, setPrice] = useState(0);
+  useEffect(() => {
+    console.log('test');
+    getPrice(setPrice);
+  }, [price]);
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <p>BTC Price: {price}</p>
       </header>
     </div>
   );
